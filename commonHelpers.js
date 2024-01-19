af23b916c3f52297e2afcd56c348e221f3098a92
YuliiaLukomska <142302232+YuliiaLukomska@users.noreply.github.com>
2024-01-19 10:39:14 +0000

    Deploying to gh-pages from @ YuliiaLukomska/goit-js-hw-11@de34fd4add50d2eaab2d64145a310362b459710a 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,24 @@
-import{i as c}from"./assets/vendor-32231325.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const i of t.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&n(i)}).observe(document,{childList:!0,subtree:!0});function r(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerpolicy&&(t.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?t.credentials="include":e.crossorigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function n(e){if(e.ep)return;e.ep=!0;const t=r(e);fetch(e.href,t)}})();const a="/goit-js-hw-11/assets/left-close-f1c537d4.svg",l={form:document.querySelector("form"),button:document.querySelector("button")},u="41896397-c8b989416d0fb53fd1030eb96",f="https://pixabay.com/api/";l.form.addEventListener("submit",m);function m(s){s.preventDefault();const o=s.currentTarget,r=o.elements.image.value;d(r).then(n=>{console.log(n),n.hits.length===0&&c.error({message:"Sorry, there are no images matching your search query. Please try again!",messageColor:"#FAFAFB",messageLineHeight:"24px",messageSize:"16px",position:"topRight",iconUrl:a,backgroundColor:"#EF4040",maxWidth:"350px",timeout:!1})}).catch(n=>console.log(n)).finally(()=>{o.reset()})}function d(s){const o=new URLSearchParams({key:u,q:s,image_type:"photo",orientation:"horizontal",safesearch:"true"});return fetch(`${f}?${o}`).then(r=>{if(r.ok)return r.json();throw new Error(r.statusText)})}
+import{i}from"./assets/vendor-32231325.js";(function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))s(t);new MutationObserver(t=>{for(const r of t)if(r.type==="childList")for(const l of r.addedNodes)l.tagName==="LINK"&&l.rel==="modulepreload"&&s(l)}).observe(document,{childList:!0,subtree:!0});function a(t){const r={};return t.integrity&&(r.integrity=t.integrity),t.referrerpolicy&&(r.referrerPolicy=t.referrerpolicy),t.crossorigin==="use-credentials"?r.credentials="include":t.crossorigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function s(t){if(t.ep)return;t.ep=!0;const r=a(t);fetch(t.href,r)}})();const c="/goit-js-hw-11/assets/left-close-f1c537d4.svg",n={form:document.querySelector("form"),button:document.querySelector("button"),list:document.querySelector("ul")},u="41896397-c8b989416d0fb53fd1030eb96",p="https://pixabay.com/api/";n.form.addEventListener("submit",f);function f(o){o.preventDefault();const e=o.currentTarget,a=e.elements.image.value;d(a).then(s=>{console.log(s),n.list.innerHTML=m(s),s.hits.length===0&&i.error({message:"Sorry, there are no images matching your search query. Please try again!",messageColor:"#FAFAFB",messageLineHeight:"24px",messageSize:"16px",position:"topRight",iconUrl:c,backgroundColor:"#EF4040",maxWidth:"350px",timeout:!1})}).catch(s=>console.log(s)).finally(()=>{e.reset()})}function d(o){const e=new URLSearchParams({key:u,q:o,image_type:"photo",orientation:"horizontal",safesearch:"true"});return fetch(`${p}?${e}`).then(a=>{if(a.ok)return a.json();throw new Error(a.statusText)})}function m(o){return o.hits.map(e=>`<li class="gallery-item">
+          <a class="gallery-link" href="${e.largeImageURL}"
+            ><img class="gallery-img" src="${e.webformatURL}" data-sourse="${e.largeImageURL}" alt="${e.tags}"
+          /></a>
+          <div class="description-wrapper">
+            <div class="value-wrapper">
+              <p class="label"><b>Likes</b></p>
+              <p class="value">${e.likes}</p>
+            </div>
+            <div class="value-wrapper">
+              <p class="label"><b>Views</b></p>
+              <p class="value">${e.views}</p>
+            </div>
+            <div class="value-wrapper">
+              <p class="label"><b>Comments</b></p>
+              <p class="value">${e.comments}</p>
+            </div>
+            <div class="value-wrapper">
+              <p class="label"><b>Downloads</b></p>
+              <p class="value">${e.downloads}</p>
+            </div>
+          </div>
+        </li>`).join("")}
 //# sourceMappingURL=commonHelpers.js.map
